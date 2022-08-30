28a6392a9cccdf3576910242d2e21bb6600f43a2
Weskley Silva <47366008+LeanWeskley@users.noreply.github.com>
2022-08-30 19:54:51 -0300

    Add files via upload

---
+++ src/matrizes/matricula.java
@@ -0,0 +1,30 @@
+package matrizes;
+
+import java.util.Scanner;
+
+public class matricula {
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int[] aluno = new int[50];
+        int[] nota = new int[50];
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Matrícula do Aluno: ");
+            aluno[i] = leia.nextInt();
+            if (aluno[i] > 0) {
+                for (int b = 0; b < 2; b++) {
+                    System.out.println("Nota do Aluno: ");
+                    int NDA = nota[b] = leia.nextInt();
+                    if (NDA >= 0 && NDA <= 10) {
+                        System.out.println("Nota válida");
+                    } else {
+                        System.out.println("Nota inválida");
+                    }
+                }
+            } else {
+                System.out.println("Não é permitido matrículas negativas!");
+            }
+        }
+
+    }
+}
