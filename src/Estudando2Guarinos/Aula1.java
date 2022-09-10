bae1adb423eb499c3ca20aeafaca13cfcce6386d
Weskley Silva <47366008+LeanWeskley@users.noreply.github.com>
2022-09-10 13:06:47 -0300

    Add files via upload

---
+++ src/Estudando2Guarinos/Aula1.java
@@ -0,0 +1,21 @@
+package Estudando2Guarinos;
+
+public class Aula1 {
+
+    public static void main(String[] args) {
+        int[] vetorzao = new int[10];
+        vetorzao[0] = 3;
+        vetorzao[1] = 4;
+        vetorzao[2] = 8;
+        vetorzao[3] = 13;
+        vetorzao[4] = 52;
+        vetorzao[5] = 22;
+        vetorzao[6] = 33;
+        System.out.println("Imprimindo...");
+        for (int i = 0; i < vetorzao.length; i++) {
+            System.out.println(vetorzao[i]);
+
+        }
+    }
+
+}
